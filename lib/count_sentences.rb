c0a1f883952b8c1b6907765c69513684e74b9ff4
Ynda Jas <yndajas@gmail.com>
2020-07-16 15:51:36 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    sentences = self.split
+    sentences = self.split(/[\s,']/)
     sentences_no_blanks = []
     sentences.each do |sentence|
       if sentence.length != 0
