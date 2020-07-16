08e5a7d7f7e09b2e401fb7b0615fc983b5700baa
Ynda Jas <yndajas@gmail.com>
2020-07-16 15:57:28 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[.!?]/)
+    sentences.select do |sentence|
+      sentence.length > 0
+    end.length
+    # sentences_no_blanks = []
+    # sentences.each do |sentence|
+    #   if sentence.length != 0
+    #     sentences_no_blanks << sentence
+    #   end
+    # end
+    # sentences_no_blanks.length   
   end
 end
